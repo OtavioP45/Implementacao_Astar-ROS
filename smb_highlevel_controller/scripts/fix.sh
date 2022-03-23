a8a0b207d65af3844d510505f1accec506ad3490
danielb <danibrigante7@gmail.com>
2022-03-23 03:26:11 -0300

    Add files via upload

---
+++ smb_highlevel_controller/scripts/fix.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+echo "Removendo o arquivo original: /opt/ros/noetic/share/velodyne_description/urdf/VLP-16.urdf.xacro"
+sudo rm -r /opt/ros/noetic/share/velodyne_description/urdf/VLP-16.urdf.xacro
+echo "Copiando o novo arquivo: ./VLP-16.urdf.xacro"
+sudo cp VLP-16.urdf.xacro /opt/ros/noetic/share/velodyne_description/urdf/
+echo "Finalizado!"
