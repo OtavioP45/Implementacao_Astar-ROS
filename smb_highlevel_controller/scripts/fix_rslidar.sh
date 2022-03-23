999ed73c3139c9595be5c845bf77e01d2f0d6fd6
danielb <danibrigante7@gmail.com>
2022-03-23 03:49:48 -0300

    Update fix_rslidar.sh

--- smb_highlevel_controller/scripts/fix_rslidar.sh
+++ smb_highlevel_controller/scripts/fix_rslidar.sh
@@ -2,5 +2,5 @@
 echo "Removendo o arquivo original: /opt/ros/noetic/share/velodyne_description/urdf/VLP-16.urdf.xacro"
 sudo rm -r /opt/ros/noetic/share/velodyne_description/urdf/VLP-16.urdf.xacro
 echo "Copiando o novo arquivo: ./VLP-16.urdf.xacro"
-sudo cp VLP-16.urdf.xacro /opt/ros/noetic/share/velodyne_description/urdf/
+sudo cp realpath "$0"/VLP-16.urdf.xacro /opt/ros/noetic/share/velodyne_description/urdf/
 echo "Finalizado!"
